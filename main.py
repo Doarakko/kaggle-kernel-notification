c36583fe916cc9aed11aae3aa21f54e33f8e4e6f
Doarakko <2wua4nlyi@gmail.com>
2019-05-09 17:33:11 +0900

    add logger

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
-import datetime
 import json
+import datetime
 import requests
+from logging import StreamHandler, INFO, DEBUG, Formatter, FileHandler, getLogger
+
 from kaggle.api.kaggle_api_extended import KaggleApi
 
 POST = os.environ['POST']
@@ -12,12 +14,28 @@ if POST == 'slack':
 elif POST == 'line':
     LINE_NOTIFY_TOKEN = os.environ['LINE_NOTIFY_TOKEN']
 
+logger = getLogger(__name__)
+log_fmt = Formatter(
+    '%(asctime)s %(name)s %(lineno)d [%(levelname)s][%(funcName)s] %(message)s')
+# info
+handler = StreamHandler()
+handler.setLevel(INFO)
+handler.setFormatter(log_fmt)
+logger.addHandler(handler)
+logger.setLevel(INFO)
+# debug
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+handler.setFormatter(log_fmt)
+logger.addHandler(handler)
+logger.setLevel(DEBUG)
+
 
 def get_kernels_url():
     api = KaggleApi()
     api.authenticate()
     kernels_list = api.kernels_list(
-        competition=COMPETITION_NAME, page_size=40, sort_by='dateCreated')
+        competition=COMPETITION_NAME, page_size=100, sort_by='dateCreated')
 
     now = datetime.datetime.utcnow()
     kernels_url = ''
@@ -30,7 +48,7 @@ def get_kernels_url():
         if last_run_date >= pre_date:
             kernels_url += 'https://www.kaggle.com/{}\n'.format(
                 getattr(kernel_info, 'ref'))
-
+    logger.debug('Get {} kernels'.format(len(kernels_list)))
     return kernels_url
 
 
@@ -49,8 +67,9 @@ def post_slack(value, title=COMPETITION_NAME):
     }
     try:
         requests.post(SLACK_WEBHOOK_URL, data=json.dumps(payload))
+        logger.debug('Post Slack')
     except Exception as e:
-        print(e)
+        logger.error(e)
 
 
 def post_line(message):
@@ -67,17 +86,15 @@ def post_line(message):
     try:
         requests.post('https://notify-api.line.me/api/notify',
                       data=payload, headers=headers)
+        logger.debug('Post LINE')
     except Exception as e:
-        print(e)
+        logger.error(e)
 
 
-def main():
+if __name__ == "__main__":
     kernels_url = get_kernels_url()
 
     if POST == 'slack':
         post_slack(value=kernels_url)
     elif POST == 'line':
         post_line(message=kernels_url)
-
-
-main()
